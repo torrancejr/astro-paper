92a6570642ad831eefe18b4805e8504000623807
Ryan <torrance.jr@gmail.com>
2024-03-10 13:20:04 -0400

    google tag take 5

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,8 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
-
+import { config } from "dotenv";
+config();
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
